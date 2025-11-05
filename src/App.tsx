371c33784913e36c992b4aec853e0f4729e54a8f
christiangarcia0311 <iyaniyan03112003@gmail.com>
2025-11-05 11:43:39 +0800

    version 0.0.2: add native platform (IOS/Android) header overlap issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
-import { IonApp, setupIonicReact } from '@ionic/react';
-import { IonRouterOutlet } from '@ionic/react';
+import { IonApp, setupIonicReact } from "@ionic/react";
+import { IonRouterOutlet } from "@ionic/react";
+import { useEffect } from "react";
 
+/* prevent overlap android */
+import { StatusBar } from "@capacitor/status-bar";
+import { Capacitor } from "@capacitor/core";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /**
  * Ionic Dark Mode
@@ -27,21 +31,32 @@ import '@ionic/react/css/display.css';
 
 /* import '@ionic/react/css/palettes/dark.always.css'; */
 /* import '@ionic/react/css/palettes/dark.class.css'; */
-import '@ionic/react/css/palettes/dark.system.css';
+import "@ionic/react/css/palettes/dark.system.css";
 
 /* Theme variables */
-import './theme/variables.css';
-
+import "./theme/variables.css";
 
 /* Tab Pages */
-import Tabs from './Tabs';
+import Tabs from "./Tabs";
 
 setupIonicReact();
 
-const App: React.FC = () => (
-  <IonApp>
-    <Tabs />
-  </IonApp>
-);
+const App: React.FC = () => {
+
+  useEffect(() => {
+    // Check if the app is running on a native platform (iOS/Android)
+    if (Capacitor.isNativePlatform()) {
+      // Set the webview NOT to overlap the status bar area.
+      // This is the core fix for the Android header overlap issue.
+      StatusBar.setOverlaysWebView({ overlay: false });
+    }
+  }, []);
+  
+  return (
+    <IonApp>
+      <Tabs />
+    </IonApp>
+  );
+};
 
 export default App;
