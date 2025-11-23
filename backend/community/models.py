4ef2383f3ed51311178767d28deca653c4675787
christiangarcia0311 <iyaniyan03112003@gmail.com>
2025-11-23 23:36:03 +0800

    version 0.0.4: create component and backend models for community management (undone)

--- backend/community/models.py
+++ backend/community/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+class CommunityGroup(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    image = models.ImageField(upload_to='community_banner_images/', null=True, blank=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_communities')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+    
+    # -- COMMUNITY SETTINGS --
+    is_private = models.BooleanField(default=False)
+    member_count = models.IntegerField(default=0)
